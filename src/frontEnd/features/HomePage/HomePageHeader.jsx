86bf90028efe29680108a0ffa12c07b87ca99351
gittest <romannadopta1@gmail.com>
2024-03-25 10:26:06 +0200

    update

--- src/frontEnd/features/HomePage/HomePageHeader.jsx
+++ src/frontEnd/features/HomePage/HomePageHeader.jsx
@@ -1,8 +1,8 @@
 import MainHeader from '../../ui/MainHeader';
 
 function HomePageHeader() {
-  const image = `../src/data/cabins/home1.jpg`;
-  // const image = `https://pazseeiqabcnmstjnbbi.supabase.co/storage/v1/object/public/cabin-images/home.jpg`;
+  // const image = `../src/data/cabins/home1.jpg`;
+  const image = `https://pazseeiqabcnmstjnbbi.supabase.co/storage/v1/object/public/cabin-images/home.jpg`;
 
   return (
     <MainHeader>
